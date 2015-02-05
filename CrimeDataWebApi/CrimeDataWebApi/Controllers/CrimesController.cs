24cd0a5ddefb1572e64e668e080f338e1aa64556
weichengyang <102356006@nccu.edu.tw>
2015-02-05 11:41:18 +0800

    testToCommit
    testToCommit

---
+++ CrimeDataWebApi/CrimeDataWebApi/Controllers/CrimesController.cs
@@ -0,0 +1,38 @@
+﻿using CrimeDataWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+
+namespace CrimeDataWebApi.Controllers
+{
+    public class CrimesController : ApiController
+    {
+        Crime[] crimes = new Crime[] 
+        { 
+            new Crime { id = 1, murderer = "Sam", victim = "Sandy", category = "knife", time = new DateTime(2014, 1, 18) }, 
+            new Crime { id = 2, murderer = "Sally", victim = "James", category = "Gun", time = new DateTime(2011, 9, 20) }, 
+            new Crime { id = 3, murderer = "Salu", victim = "Salu", category = "kill", time = new DateTime(2010, 5, 20) },
+            new Crime { id = 4, murderer = "Sam", victim = "Sam", category = "knife", time = new DateTime(2014, 1, 18) }, 
+            new Crime { id = 5, murderer = "Sally", victim = "Sally", category = "Gun", time = new DateTime(2011, 9, 20) }, 
+            new Crime { id = 6, murderer = "Salu", victim = "Salu", category = "kill", time = new DateTime(2010, 5, 20) } 
+        };
+
+        public IEnumerable<Crime> GetAllCrimes()
+        {
+            return crimes;
+        }
+
+        public IHttpActionResult GetCrime(int id)
+        {
+            var crime = crimes.FirstOrDefault((c) => c.id == id);
+            if (crime == null)
+            {
+                return NotFound();
+            }
+            return Ok(crime);
+        }
+    }
+}
---
+++ CrimeDataWebApi/CrimeDataWebApi/Models/Crime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrimeDataWebApi.Models
+{
+    public class Crime
+    {
+        public int id { get; set; }
+        public string murderer { get; set; }
+        public string victim { get; set; }
+        public DateTime time { get; set; }
+        public string category { get; set; }
+
+    }
+
+}
